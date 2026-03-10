4e6c3c122c1711c467a036984f1f7b0b1e0b9649
jittipat2002x-sys <jittipat2002x@gmail.com>
2026-03-10 20:41:07 +0700

    update-db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,7 +4,12 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 import * as bcrypt from 'bcrypt';
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({ 
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
